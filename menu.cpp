cbc1cda6e6fc03022be306e9f2e62103de4dc6a5
Yadzka <yadzkaahza@gmail.com>
2025-05-15 00:58:56 +0800

    menambahkan fungsi Input

--- menu.cpp
+++ menu.cpp
@@ -2,23 +2,41 @@
 #include <conio.h>
 using namespace std;
 
+const int MAX = 10; // konstanta jumlah data
 
-void dMenu(){
-system("cls");
-cout<<"Aplikasi Tampilan Menu"<<"\n";       
-cout<<"1. Menu Pertama"<<"\n";            
-cout<<"2. Menu Kedua"<<"\n";            
-cout<<"3. Menu Ketiga"<<"\n";           
-cout<<"4. Menu Keempat"<<"\n";            
-cout<<"5. Exit"<<"\n";           
-cout<<"Masukan angka :";        
-
+void dMenu()
+{
+  system("cls");
+  cout << "Aplikasi Tampilan Menu" << "\n";
+  cout << "1. Menu Pertama" << "\n";
+  cout << "2. Menu Kedua" << "\n";
+  cout << "3. Menu Ketiga" << "\n";
+  cout << "4. Menu Keempat" << "\n";
+  cout << "5. Exit" << "\n";
+  cout << "Masukan angka :";
 }
 
-void mPertama(string pesan){
-system("cls");
-cout<<"hallo saya menu "<<pesan;
-getch();
+void mPertama(string pesan)
+{
+  system("cls");
+  cout << "hallo saya menu " << pesan;
+  getch();
+}
+
+// Fungsi user menginput data
+void inputData(int data[], int n)
+{
+  system("cls");
+  cout << "Menu ke-1" << endl;
+  cout << "Masukkan 10 data : " << endl;
+
+  for (int i = 0; i < n; i++)
+  {
+    cout << "Masukkan Data ke-" << i + 1 << ": ";
+    cin >> data[i];
+  }
+  cout << "Tekan tombol apapun untuk kembali ke menu utama." << endl;
+  getch();
 }
 
 // Fungsi quote / sepatah kata
@@ -30,42 +48,36 @@ void sepatahKata()
   getch();
 }
 
-int main() {
-char pl;
-do
+int main()
 {
-    dMenu();
-    pl=getch();
-  switch (pl)
+  char p;
+  int d_array[MAX];
+  do
   {
-   case '1':
-    /* code */
-    mPertama("pertama");
-    break;
-   case '2':
-    mPertama("ke- dua");
-    /* code */ 
-    break;  
-   case '3':
-    mPertama("ke- tiga");
-    /* code */
-    break;  
-   case '4':
-    sepatahKata();
-    /* code */
-    break;  
-  case '5':
-    /* code */
-    break;
-  
-  default:
-    system("cls");
-    cout<<"Pilihan Tidak Tersedia";
-    getch();
-    break;
-  }
+    dMenu();
+    p = getch();
+    switch (p)
+    {
+    case '1':
+      inputData(d_array, MAX);
+      break;
+    case '2':
 
+      break;
+    case '3':
 
-} while (pl!='5');
+      break;
+    case '4':
+      sepatahKata();
+      break;
+    case '5':
+      break;
+    default:
+      system("cls");
+      cout << "Pilihan Tidak Tersedia";
+      getch();
+      break;
+    }
+  } while (p != '5');
   return 0;
 }
